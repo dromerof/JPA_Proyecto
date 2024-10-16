d54b8a7332bb6bdb6a9f8ad34badea48eaff7c28
Daniel Romero <dandres508@gmail.com>
2024-10-16 13:02:59 -0500

    Creacion de la entity employee

--- src/main/java/org/dromerof/entity/Employee.java
+++ src/main/java/org/dromerof/entity/Employee.java
@@ -1,4 +1,83 @@
 package org.dromerof.entity;
 
 public class Employee {
+
+    private int id;
+    private String firstName;
+    private String paSurname;
+    private String maSurname;
+    private String email;
+    private float salary;
+
+    public Employee() {
+    }
+
+    public Employee(int id, String firstName, String paSurname, String maSurname, String email, float salary) {
+        this.id = id;
+        this.firstName = firstName;
+        this.paSurname = paSurname;
+        this.maSurname = maSurname;
+        this.email = email;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getPaSurname() {
+        return paSurname;
+    }
+
+    public void setPaSurname(String paSurname) {
+        this.paSurname = paSurname;
+    }
+
+    public String getMaSurname() {
+        return maSurname;
+    }
+
+    public void setMaSurname(String maSurname) {
+        this.maSurname = maSurname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public float getSalary() {
+        return salary;
+    }
+
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", paSurname='" + paSurname + '\'' +
+                ", maSurname='" + maSurname + '\'' +
+                ", email='" + email + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
 }
